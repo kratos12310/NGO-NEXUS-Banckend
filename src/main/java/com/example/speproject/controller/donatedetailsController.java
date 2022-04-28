cbb4e9e37e82e277182e115b6718f3f750def489
AsthaBorkataky <asthaborkataky@gmail.com>
2022-04-28 14:32:04 +0530

    log details

--- src/main/java/com/example/speproject/controller/donatedetailsController.java
+++ src/main/java/com/example/speproject/controller/donatedetailsController.java
@@ -3,6 +3,8 @@ package com.example.speproject.controller;
 import com.example.speproject.bean.DonateDetails;
 import com.example.speproject.bean.Ngo;
 import com.example.speproject.service.donatedetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +14,18 @@ import java.util.List;
 @RequestMapping("api/")
 @CrossOrigin("*")
 public class donatedetailsController {
+    Logger logger= LoggerFactory.getLogger(donatedetailsController.class);
 @Autowired
     private donatedetailsService donatedetailsservice;
 
     @PostMapping("/savedonatedetails")
     public DonateDetails saveDonateDetails(@RequestBody DonateDetails donatedetails) {
+       logger.info("Saving Donation Details");
         return donatedetailsservice.saveDonateDetails(donatedetails);
     }
     @GetMapping("/getdonatedngo/{id}")
     public DonateDetails getDonateDetailsById(@PathVariable("id") Long id){
+        logger.info("Getting Donation Details by id "+id);
         return donatedetailsservice.getdonatedetailsbyid(id);
     }
 }
